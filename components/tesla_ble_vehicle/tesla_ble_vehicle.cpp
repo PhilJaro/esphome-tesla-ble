2fbf6a84820c2ec1ecb3d23895febf00ff7414fb
PedroKTFC <giordys@gmail.com>
2026-07-27 22:17:19 +0100

    Undo unintended change

--- components/tesla_ble_vehicle/tesla_ble_vehicle.cpp
+++ components/tesla_ble_vehicle/tesla_ble_vehicle.cpp
@@ -2290,8 +2290,7 @@ namespace esphome
           ESP_LOGE(TAG, "reg for notify failed, error status = %x", param->reg_for_notify.status);
           break;
         }
-//        this->node_state = espbt::ClientState::ESTABLISHED;
-this->set_state (espbt::ClientState::ESTABLISHED);
+        this->node_state = espbt::ClientState::ESTABLISHED;
         this->status_clear_warning();
         ble_disconnected_ = BleConnected;
         number_updates_since_connection_ = 0; //Reset update loop counter
